b2725939b57e02795c78e4e8aabbb2e5fff36269
Yovj <614000736@qq.com>
2019-11-19 19:41:07 +0800

    neww

--- Blog/apps/Article/views.py
+++ Blog/apps/Article/views.py
@@ -365,10 +365,11 @@ def get_blogList(request):
         return restful.fail(message="用户不存在")
 
     if isHome == True: # 需要该用户的博文和关注用户发表和推荐的博文
-        focused_user = User.objects.filter(who_relation_set__relation_type=1)
-        # print(focused_user)
+        focused_user = User.objects.filter(relation_who_set__relation_type=1,relation_who_set__who_relation=user)
+        print(focused_user)
         focused_user_blog = Article.objects.filter( ~Q(author=user) & (Q(author__in=focused_user) | Q(recommand_detail__user__in=focused_user))).all().order_by("-pub_time")
         # print(focused_user_blog.query)
+        print(focused_user_blog)
         user_blog = Article.objects.filter(author=user).all().order_by("-pub_time") # 该用户的博文
         serializer = BlogDetail_Serializer(focused_user_blog,many=True)
         blog_data = serializer.data
